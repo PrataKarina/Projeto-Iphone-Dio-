c3283f3e44bccd79ae990956cdc7a67484d767d7
PrataKarina <155679408+PrataKarina@users.noreply.github.com>
2024-01-08 16:53:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using DesafioPOO.Models;
+
+Nokia celular1 = new Nokia("911110000","nokia 2", "0000123456", 12);
+celular1.Ligar();
+celular1.ReceberLigacao();
+celular1.InstalarAplicativo("newApp");
+
+Iphone phone1 = new Iphone("900001111","iphone 11", "000078910", 24);
+phone1.Ligar();
+phone1.ReceberLigacao();
+phone1.InstalarAplicativo("newApp");
